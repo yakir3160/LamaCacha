bb01ceefecc3fc058be053f5f8c1fc5d87571a84
yakir3160 <yakir2062@gmail.com>
2025-12-29 21:32:20 +0200

    Refactor MapWindow component: rename to PascalCase, improve structure by wrapping MapView in a container

--- components/device/mapWindow.js
+++ components/device/mapWindow.js
@@ -1,34 +1,38 @@
-
-import {MapView, Marker} from 'react-native-maps';
-
-
+import MapView, {Marker} from 'react-native-maps';
+import {StyleSheet, View} from 'react-native';
 
 
 
-
-export const mapWindow = ({ latitude, longitude }) => {
+export const MapWindow = ({latitude, longitude}) => {
     return (
-        <MapView
-            style={styles.map}
-            initialRegion={{
-                latitude: latitude,
-                longitude: longitude,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-            }}
-        >
-            <Marker coordinate={{ latitude, longitude }} />
-        </MapView>
+        <View style={styles.container}>
+            <MapView
+                style={styles.map}
+                initialRegion={{
+                    latitude: latitude,
+                    longitude: longitude,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+            >
+                <Marker coordinate={{latitude, longitude}} />
+            </MapView>
+        </View>
     );
 };
 
 
 
-const styles = {
-    map: {  
+const styles = StyleSheet.create({
+    container: {
+        height: 200,
+        width: '100%',
+        marginTop: 20,
         borderRadius: 10,
-        flex: 1,
         overflow: 'hidden',
     },
-};
+    map: {
+        ...StyleSheet.absoluteFillObject,
+    },
+});
 
